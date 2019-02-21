433908ecd459f1dda54b26f538e250a7e59c3505
Natalie Halawani <hala1224@gmail.com>
2019-02-21 19:19:40 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,15 +7,6 @@ attr_accessor :id, :name, :type, :hp, :db
 
 
 
-  @@all =[]
-  
-  def initialize(id, name, type, db)
-    @id=id
-    @name=name
-    @type=type
-    @db=db
-    @@all << self
-  end
   
   def self.save
   end  
